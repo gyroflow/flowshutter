2a5928f0affa20ef322a1ae316c53305f9b6923d
Hugo Chiang <31283897+DusKing1@users.noreply.github.com>
2022-02-09 11:33:58 +0800

    refactor sony mtp uart

--- sony_multiport.py
+++ sony_multiport.py
@@ -13,6 +13,8 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with flowshutter.  If not, see <https://www.gnu.org/licenses/>.
+import uasyncio as asyncio
+import target,oled
 def init_multiport_packet():
     rcd_prs = b'#7100*'
     rcd_rls = b'#7110*'
@@ -27,3 +29,32 @@ def init_multiport_packet():
     cm_rcd_stop_ack = b'&76000*'
 
     return rcd_prs, rcd_rls, cm_hdsk, cm_hdsk_ack, cm_rcd_start, cm_rcd_start_ack, cm_rcd_stop, cm_rcd_stop_ack
+
+async def uart_handler():
+    rcd_prs, rcd_rls, cm_hdsk, cm_hdsk_ack, cm_rcd_start, cm_rcd_start_ack, cm_rcd_stop, cm_rcd_stop_ack = init_multiport_packet()
+    _, uart2, _, _, _ = target.init_pins()
+    oled1 = oled.oled_init()
+    global switching_flag
+    global arm_flag
+    swriter = asyncio.StreamWriter(uart2, {})
+    sreader = asyncio.StreamReader(uart2)
+    while True:
+        res = await sreader.read(n=-1)
+        print('Cam sent:', res)
+        if res == cm_hdsk: # receive handshake
+            await asyncio.sleep_ms(10)
+            await swriter.awrite(cm_hdsk_ack) # send handshake ack to camera
+        elif res == cm_rcd_start:
+            await asyncio.sleep_ms(9)# I don't know if this timing is good, should look into it later
+            switching_flag = 0
+            arm_flag = 1
+            await swriter.awrite(cm_rcd_start_ack) # send record start ack to camera
+            print('camera is recording')
+            oled.show_arm_info(oled1)
+        elif res == cm_rcd_stop:
+            await asyncio.sleep_ms(10)
+            switching_flag = 0
+            arm_flag = 0
+            await swriter.awrite(cm_rcd_stop_ack) # send record stop ack to camera
+            print('camera is stopped')
+            oled.show_disarm_info(oled1)
