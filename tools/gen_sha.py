f5c886ce4ae0de3eeec49f0aaa24154e86391635
Hugo Chiang <31283897+DusKing1@users.noreply.github.com>
2022-04-13 15:45:59 +0800

    add check option for gen_sha

--- tools/gen_sha.py
+++ tools/gen_sha.py
@@ -13,10 +13,15 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with flowshutter.  If not, see <https://www.gnu.org/licenses/>.
-import hashlib, json, os
+import hashlib, json, os, sys
 
-sha1_hash = hashlib.sha1()
+sha1 = hashlib.sha1()
 files = os.listdir("src/")
+
+try:
+    files.remove("sha.json")
+except ValueError:
+    pass
 
 # print(files)
 jtext = {"files":[]}
@@ -24,13 +29,22 @@ jtext = {"files":[]}
 for f in files:
     with open("src/"+f,"rb") as hf:
         for byte_block in iter(lambda: hf.read(4096),b""):
-            sha1_hash.update(byte_block)
-        # print(f,sha1_hash.hexdigest()) # for debug
-        jtext["files"].append({"name":f,"sha1":sha1_hash.hexdigest()})
+            sha1.update(byte_block)
+        # print(f,sha1.hexdigest()) # for debug
+        jtext["files"].append({"name":f,"sha1":sha1.hexdigest()})
 
 jdata = json.dumps(jtext,indent = 4, separators=(',', ': '))
+
+jdir = ""
+# print(len(sys.argv)) # for debug
+if len(sys.argv) > 1:
+    if sys.argv[1] == "check":
+        jdir = "check_sha.json"
+else:
+    jdir = "src/sha.json"
+    
 print("SHA1 of all files generated!")
-jfile = open("src/sha.json","w")
+jfile = open(jdir,"w")
 jfile.write(jdata)
 jfile.close()
 print("Update sha.json success!")
