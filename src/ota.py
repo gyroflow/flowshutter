977e374dd2a68138a3ed801e3f0cb44096208024
Hugo Chiang <31283897+DusKing1@users.noreply.github.com>
2022-05-08 22:50:46 +0800

    add ota fetch

--- src/ota.py
+++ src/ota.py
@@ -13,7 +13,10 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with flowshutter.  If not, see <https://www.gnu.org/licenses/>.
-import json, urequests, vars
+import json, urequests, vars, os
+
+update_list = []
+delete_list = []
 
 def build_url(file_name):
     source = ""
@@ -31,30 +34,20 @@ def build_url(file_name):
     elif vars.ota_channel == "dev":
         channel = "master"
     
-    # print("source:" + source)
-    # print("channel:" + channel)
     remote_url = source + channel + "/src/" + file_name
     return remote_url
 
-def build_sha_url():
+def check():
+    remote_sha_url = build_url("sha.json")
     # https://raw.githubusercontent.com/gyroflow/flowshutter/master/src/sha.json
     # https://gitee.com/dusking1/flowshutter/raw/master/src/sha.json
-    remote_sha_url = build_url("sha.json")
-    return remote_sha_url
 
-
-def check():
-    remote_sha_url = build_sha_url()
     print("hello! Now trying to get remote SHA...")
     print("remote SHA url: " + remote_sha_url)
     
-    #remote_sha = urequests.get(remote_sha_url).json()
     remote_sha = urequests.get(remote_sha_url)
     print("remote SHA downloaded")
     sha_jdata = remote_sha.json()
-    #### this is for debugging
-    # print(type(remote_sha))
-    # print(remote_sha.text)
     with open("tmp_sha.json", "w") as f:
         json.dump(sha_jdata, f)
         print("Saved remote SHA to tmp_sha.json")
@@ -65,6 +58,7 @@ def check():
     #### can be removed later
 
 def compare():
+    global update_list
     with open("tmp_sha.json", "r") as f_1:
         upstream = json.load(f_1)
         f_1.close()
@@ -75,9 +69,9 @@ def compare():
     # print("upstream: " + str(upstream))
     # print("local: " + str(local))
 
-    for i in range(1,len(local['files'])):
+    for i in range(0,len(local['files'])):
         # compare local => upstream to get update and delete info
-        for j in range(1,len(upstream['files'])):
+        for j in range(0,len(upstream['files'])):
             if local['files'][i]['name'] == upstream['files'][j]['name']:
 
                 # A file with the same name is found both locally and remotely,
@@ -89,16 +83,18 @@ def compare():
                 else:
                     # Local file is outdated, it should be updated
                     print(str(local['files'][i]['name']) + " outdated")
+                    update_list.append(local['files'][i]['name'])
                     break
 
             elif j == len(upstream['files']) - 1 and local['files'][i]['name'] != upstream['files'][j]['name']:
                 # The local file was not found in the remote
                 # it should be deleted
                 print(str(local['files'][i]['name']) + " deleted")
+                delete_list.append(local['files'][i]['name'])
     
-    for i in range(1,len(upstream['files'])):
+    for i in range(0,len(upstream['files'])):
         # compare upstream => local to get new file info
-        for j in range(1,len(local['files'])):
+        for j in range(0,len(local['files'])):
             if upstream['files'][i]['name'] == local['files'][j]['name']:
                 # we have checked this before
                 break
@@ -106,3 +102,49 @@ def compare():
                 # The remote file was not found in the local
                 # It is the new file, which should be downloaded
                 print(str(upstream['files'][i]['name']) + " new")
+                update_list.append(upstream['files'][i]['name'])
+    
+    print("Delete list: "+str(delete_list))
+    if len(delete_list) !=0:
+        jdelete = {"files": []}
+        for i in range(0,len(delete_list)):
+            jdelete['files'].append({"name": str(delete_list[i])})
+        jdelete_data = json.dumps(jdelete)
+        with open("delete_list.json", "w") as f:
+            f.write(jdelete_data)
+            f.close()
+
+    print("Update list: "+str(update_list))
+    if len(update_list) !=0:
+        jupdate = {"files": []}
+        for i in range(0,len(update_list)):
+            jupdate["files"].append({"name": str(update_list[i])})
+        jupdate_data = json.dumps(jupdate)
+        with open("update_list.json", "w") as f:
+            f.write(jupdate_data)
+            f.close()
+        fetch()
+
+
+def fetch():
+    global update_list
+    print("try to creat tmp dir")
+    try:
+        os.mkdir("tmp")
+    except:
+        pass
+    
+    print("now try to fetch files")
+    for i in range(0,len(update_list)):
+        print(str(update_list[i]) + " downloading...")
+        url=build_url(str(update_list[i]))
+        print(url)
+        content = urequests.get(url)
+        with open("tmp/"+update_list[i], "w") as f:
+            f.write(content.text)
+            print(str(update_list[i])+ " is written to tmp")
+            f.close()
+        del f
+        del url
+        del content
+    print(os.listdir('tmp'))
