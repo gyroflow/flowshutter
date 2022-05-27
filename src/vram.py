e0e3964cfa3a1a98cdbf7b70466e40bbbb2675e7
Hugo Chiang <31283897+DusKing1@users.noreply.github.com>
2022-05-27 08:04:20 +0800

    bump 0.61

--- src/vram.py
+++ src/vram.py
@@ -53,7 +53,7 @@ oled_need_update = "no"
 
 
 # user_settings:
-version = "0.60" # when new user settings added, this should be update firstly!
+version = "0.61" # when new user settings added, this should be update firstly!
 device_mode = "SLAVE"
 inject_mode = "OFF"
 camera_protocol = "Sony MTP"
